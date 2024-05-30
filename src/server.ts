71ce4279f1900125cbbbec60a61b2e65a69f5395
asif-mohmd <asifasifpsps@gmail.com>
2024-05-30 22:09:37 +0530

    Fixed conflick merge after console.log removed

--- src/server.ts
+++ src/server.ts
@@ -41,7 +41,8 @@ const grpcServer = () => {
     server.addService((adminProto.AdminService as any).service, {
 
         Login : controller.onLogin.bind(controller),
-        AddCategory : controller.addCategory.bind(controller)
+        AddCategory : controller.addCategory.bind(controller),
+        GetAllCategories : controller.getAllCategories.bind(controller)
     
         
         // Implementation of service methods
